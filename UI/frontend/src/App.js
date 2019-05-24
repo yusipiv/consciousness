7c4dcaef603ca5baccfd9e329819446a39741a28
yyusypiv@luxoft.com <yyusypiv@luxoft.com>
2019-05-24 12:47:55 +0300

    refactored app + recreate connection after socket shutdown

--- UI/frontend/src/App.js
+++ UI/frontend/src/App.js
@@ -14,42 +14,55 @@ class App extends React.Component {
     super(props);
     this.URL = 'http://localhost:39099/gs-guide-websocket/';
     this.stompClient = null;
-    this.socket = null;
-    this.prices = {};
+    this.connected = false;
   }
 
-  static setConnected(connected) {
+  setConnected(connected) {
+    this.connected = connected;
     console.log(connected ? 'Socket Connected!' : 'Socket Disconnected!');
   }
 
   connect() {
-    this.socket = new SockJS(this.URL);
-    this.stompClient = Stomp.over(this.socket);
-    this.stompClient.debug = null;
-    this.stompClient.connect({}, frame => {
-      App.setConnected(true);
-      this.stompClient.subscribe('/topic/prices', rateValue => {
-        let rates = JSON.parse(rateValue.body);
-        let instrument = `${rates.ccy}/${rates.base_ccy}`;
-        this.prices[instrument] = {
-          'instrument': instrument,
-          'buy': rates.buy,
-          'sale': rates.sale
-        };
-        this.setState({prices: this.prices});
-      });
-
-      this.stompClient.subscribe('/topic/midValue', response =>
-        this.setState({midValue: JSON.parse(response.body)})
-      );
-    });
+    this.stompClient = Stomp.over(new SockJS(this.URL));
+    //this.stompClient.debug = null;
+    this.stompClient.connect({}, this.onConnect, this.onDisconnect);
   }
+
+  onConnect = () => {
+    this.setConnected(true);
+    this.stompClient.subscribe('/topic/prices', this.pricesSubscription());
+    this.stompClient.subscribe('/topic/midValue', this.simulatorSubscription);
+  };
+
+  pricesSubscription = () => {
+    let prices = {};
+    return (rateValue) => {
+      let rates = JSON.parse(rateValue.body);
+      let instrument = `${rates.ccy}/${rates.base_ccy}`;
+      prices[instrument] = {
+        'instrument': instrument,
+        'buy': rates.buy,
+        'sale': rates.sale
+      };
+      this.setState({prices: prices});
+    };
+  };
+
+  simulatorSubscription = (response) => {
+    this.setState({midValue: JSON.parse(response.body)});
+  };
+
+  onDisconnect = (err) => {
+    console.log(err);
+    this.setConnected(false);
+    setTimeout(this.connect, 5000);
+  };
 
   disconnect() {
     if (this.stompClient !== null) {
       this.stompClient.disconnect();
     }
-    App.setConnected(false);
+    this.setConnected(false);
   }
 
   componentDidMount() {
